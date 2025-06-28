1101e89180b1dd71b67454d7f83aebb6647383e3
maksat1717 <131411713+maksat1717@users.noreply.github.com>
2025-06-27 23:15:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def driver_dashboard(request: Request):
     return templates.TemplateResponse("driver.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=3000)
